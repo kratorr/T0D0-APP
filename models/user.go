269f13ca7d1a30cfcdcd5d67a007f0884a733276
kratorr <smirnovv91@gmail.com>
2022-05-07 10:38:22 +0300

    Add todoList model

--- models/user.go
+++ models/user.go
@@ -5,3 +5,13 @@ type User struct {
 	Login    string `json:"login" binding:"required"`
 	Password string `json:"password" binding:"required"`
 }
+
+type TodoList struct {
+	Title       string `json:"title" binding:"required"`
+	Description string `json:"description" binding:"required"`
+}
+
+// type TodoItem struct {
+// 	Title       string `json:"title" binding:"required"`
+
+// }
